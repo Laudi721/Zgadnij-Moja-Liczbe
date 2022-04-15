befe92a73323f1a4cfeffe84727a470ca3984423
Lesniak <lesniak721@gmail.com>
2022-04-15 18:42:52 +0200

    pierwsze metody HTTP

---
+++ Zgadnij Moja Liczbe/GuessNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zgadnij_Moja_Liczbe
+{
+    public class GuessNumber
+    {
+        public string id { get; set; }
+        public int count { get; set; }
+        public int number { get; set; }
+    }
+}
